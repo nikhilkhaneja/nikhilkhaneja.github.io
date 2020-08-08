125051909c4d2518d64de17b0b759e9d541cd5a6
NIKHIL KISHAN KHANEJA <36797736+nikhilkhaneja@users.noreply.github.com>
2020-08-08 18:05:00 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,5 @@
-
 var user;
 var selectedFile;
-
-
 
 $("#file").on("change", function(event) {
 	selectedFile = event.target.files[0];
@@ -10,7 +7,6 @@ $("#file").on("change", function(event) {
 });
 
 function uploadFile() {
-	// Create a root reference
 	var filename = selectedFile.name;
 	var storageRef = firebase.storage().ref('/Images/' + filename);
 	var uploadTask = storageRef.put(selectedFile);
@@ -27,35 +23,34 @@ function uploadFile() {
 	}, function() {
 	  // Handle successful uploads on complete
 	  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-	  var str;
+		var str;
 	 
-	  var user_id;
-	  var url;
-	  var caption;
- 	uploadTask.snapshot.ref.getDownloadURL().then( 
-        function (downloadURL) { 
+	  	var user_id;
+	 	 var url;
+	  	var caption;
+ 	  	uploadTask.snapshot.ref.getDownloadURL().then( 
+        	function (downloadURL) { 
         	$.fn.getFormData = function() {
-      var fields = this.find('[name]');
-      var result = {};
-      $.each(fields, function (i, el) {
-        result[el.name] = el.value;
-      });
-      return result;
-    };
-        var updates = {};
-        user_id = user_id ? user_id : firebase.database().ref().child('user-data').push().key;
-        console.log('File available at', downloadURL); 
-        tr = downloadURL;
-        console.log(downloadURL); 
-        var postData = {
-        	Image_URL : downloadURL,
-        	Data:  $('form').getFormData()
-        }
-        updates['/post' + user_id] = postData;
-        firebase.database().ref().child('user-data').update(updates);
-	 });
+      			var fields = this.find('[name]');
+      			var result = {};
+      			$.each(fields, function (i, el) {
+        			result[el.name] = el.value;
+      			});
+      		return result;
+    		};
+        	var updates = {};
+        	user_id = user_id ? user_id : firebase.database().ref().child('user-data').push().key;
+        	console.log('File available at', downloadURL); 
+        	tr = downloadURL;
+        	console.log(downloadURL); 
+        	var postData = {
+        		Image_URL : downloadURL,
+        		Data:  $('form').getFormData()
+        	}
+        	updates['/post' + user_id] = postData;
+        	firebase.database().ref().child('user-data').update(updates);
+	 	});
  	});
-
 }
 
 
