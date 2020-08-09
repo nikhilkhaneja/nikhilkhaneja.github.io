f5c951e5f783b32091fe6220d3f7cc4a614cb748
NIKHIL KISHAN KHANEJA <36797736+nikhilkhaneja@users.noreply.github.com>
2020-08-09 15:38:47 +0530

    Update mysecond.js

--- mysecond.js
+++ mysecond.js
@@ -1,10 +1,10 @@
-
+var user_id
 fileButton.addEventListener('change',function(e){
 
 	for(let i = 0;i<e.target.files.length;i++){
 
 		var imageFile = e.target.files[i];
-		var storageRef = firebase.storage().ref('Images/' + imageFile.name);
+		var storageRef = firebase.storage().ref('Images' + imageFile.name);
 		var task = storageRef.put(imageFile);
 		task.on('state_changed',function progress(snapshot){
 
@@ -28,7 +28,26 @@ fileButton.addEventListener('change',function(e){
   
               // print the image url  
                console.log(downloadURL); 
-         
+               $.fn.getFormData = function() {
+      			var fields = this.find('[name]');
+      			var result = {};
+      			$.each(fields, function (i, el) {
+        			result[el.name] = el.value;
+      			});
+      		return result;
+    		};
+    		
+         var updates = {};
+        	user_id = user_id ? user_id : firebase.database().ref().child('user-data').push().key;
+        	console.log('File available at', downloadURL); 
+        	tr = downloadURL;
+        	console.log(downloadURL); 
+        	var postData = {
+        		Image_URL : downloadURL,
+        		Data:  $('form').getFormData()
+        	}
+        	updates['/post' + user_id] = postData;
+        	firebase.database().ref().child('user-data').update(updates);
             }); 
 
 		})
