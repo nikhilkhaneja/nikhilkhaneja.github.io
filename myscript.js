ed5a1f70a37a5aaccef5c9e5971e676d0188d8f4
Nikhil Khaneja <36797736+nikhilkhaneja@users.noreply.github.com>
2020-08-18 00:40:03 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -12,13 +12,28 @@ const config =
         measurementId: "G-5EB12TGK2N"
     };
     firebase.initializeApp(config);
+    var elem = document.getElementById("myBar");   
+    var width = 1;
+    var id = setInterval(frame, 40);
+    function frame() 
+    {
+        if (width >= 100) 
+        {
+           
+            clearInterval(id);
         
     // Get a reference to the storage service, which is used to create references in your storage bucket
+    
+    
     const storage = firebase.storage();
 
     // Create a storage reference from our storage service
     const storageRef = storage.ref();
     $('#ist').find('tbody').html('');
+
+    var new_htm = '';
+    new_htm = '<h3 class="mbr-section-subtitle align-center pb-5 mbr-light mbr-fonts-style display-5">The Images Retrieved are:</h3>';
+    $('#ist').find('tbody').append(new_htm);
 
 var i = 0;
 
@@ -38,14 +53,22 @@ storageRef.child('multiple-image-'+fn+'/').listAll().then(function(result){
 });
 function displayImage(row, images){
   var new_html = '';
-  images.getDownloadURL().then(function(url){
+    images.getDownloadURL().then(function(url){
+      
 
-      new_html += "<div>";
-      new_html += '<img width = "500" height = "500" src = '+url+'>';
+      new_html += '<div style= "text-align : center">';
+      new_html += '<img width = "270" height = "270" src = '+url+'>';
+      new_html += '<br><br>';
       new_html += "</div>";
 
       $('#ist').find('tbody').append(new_html);
   });
 }
-
+} 
+else 
+{
+    width++; 
+    elem.style.width = width + '%'; 
+}
+} 
   }
